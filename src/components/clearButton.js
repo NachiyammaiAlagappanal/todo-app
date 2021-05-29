6b5715432dadc9552ae7fa3da96d5d3351e99536
Vishal Naryanan <vishal.n.2011@gmail.com>
2021-05-29 10:30:38 +0530

    Hide the clear completed button.

--- src/components/clearButton.js
+++ src/components/clearButton.js
@@ -1,7 +1,20 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable indent */
 import { React } from 'react';
 import context from '../core/context';
+import TodoManager from '../services/TodoManager';
 
-const clearButton = () =>
-	<button onClick={ context.actions.clearCompleted }>Clear Completed</button>;
+const clearButton = () => {
+	const { todos } = context.state;
+	const noclearTodo = TodoManager.getClearCount(todos) === 0;
+
+	return noclearTodo
+		? null
+		: <button
+				onClick={ context.actions.clearCompleted }
+			>
+			Clear Completed
+		</button>;
+};
 
 export default clearButton;
